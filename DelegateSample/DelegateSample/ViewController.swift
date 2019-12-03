7e9b3bc319cc5c756c9955bb482c8980de9f1553
TomohikoHirose <hirose@agentec.jp>
2019-12-03 13:25:03 +0900

    delegate pattern initial commit

---
+++ DelegateSample/DelegateSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DelegateSample
+//
+//  Created by hirose on 2019/12/03.
+//  Copyright © 2019 hirose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
